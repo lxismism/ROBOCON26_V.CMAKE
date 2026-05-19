b158064020fe655d4fff82a1842be89460e8c2ad
TJceleste <3561723394@qq.com>
2026-05-20 03:01:33 +0800

    注释屏蔽实例/测试程序

--- APP/control_task/control_task.cpp
+++ APP/control_task/control_task.cpp
@@ -265,12 +265,12 @@ void controlTask(void *argument) {
         if(control_ir_sub.TryGet(&control_ir_msg)) {
           //红外信号处理放这
           //测试
-          if(control_ir_msg.data1 == 0x2B && control_ir_msg.data2 == 0xFC)
-          {
-            ir_cmd.tx_data[0] = 0x67; //示例：接收到特定红外信号后，发送0x01命令
-            ir_cmd.tx_data[1] = 0x78; //示例：接收到特定红外信号后，发送0x02命令
-            ir_cmd_pub.Publish(ir_cmd);
-          }
+          // if(control_ir_msg.data1 == 0x2B && control_ir_msg.data2 == 0xFC)
+          // {
+          //   ir_cmd.tx_data[0] = 0x67; //示例：接收到特定红外信号后，发送0x01命令
+          //   ir_cmd.tx_data[1] = 0x78; //示例：接收到特定红外信号后，发送0x02命令
+          //   ir_cmd_pub.Publish(ir_cmd);
+          // }
         }
 
         vTaskDelayUntil(&currentTime, 5);
