25952588fcc7b30fc7ab8f06391a3e702aa923a3
TJceleste <3561723394@qq.com>
2026-05-19 13:13:48 +0800

    启用usart10

--- Core/Inc/stm32h7xx_it.h
+++ Core/Inc/stm32h7xx_it.h
@@ -69,7 +69,10 @@ void DMA1_Stream7_IRQHandler(void);
 void UART4_IRQHandler(void);
 void UART5_IRQHandler(void);
 void TIM6_DAC_IRQHandler(void);
+void DMA2_Stream0_IRQHandler(void);
+void DMA2_Stream1_IRQHandler(void);
 void OTG_HS_IRQHandler(void);
+void USART10_IRQHandler(void);
 void FDCAN3_IT0_IRQHandler(void);
 void FDCAN3_IT1_IRQHandler(void);
 /* USER CODE BEGIN EFP */
