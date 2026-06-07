f1d913588602d92d6ba1d866b082eb4a4ea3e52a
Tim <2055144074@qq.com>
2026-06-07 21:21:48 +0800

    缓解超调

--- APP/chassis_task/chassis_task.cpp
+++ APP/chassis_task/chassis_task.cpp
@@ -53,7 +53,7 @@ const std::array<OmniChassis::SpeedPidParam, OmniChassis::kWheelCount>
         // OmniChassis::SpeedPidParam(1200.0f, 750.0f, 0.0f, 16000.0f, 0.5f, NONE), // 右上
         // OmniChassis::SpeedPidParam(1200.0f, 750.0f, 0.0f, 16000.0f, 0.5f, NONE), // 左下
         // OmniChassis::SpeedPidParam(1200.0f, 750.0f, 0.0f, 16000.0f, 0.5f, NONE), // 右下
-        OmniChassis::SpeedPidParam(1300.0f, 1800.0f, 0.0f, 16000.0f, 0.0f, IMCREATEMENT_OF_OUT), // 左上
+        OmniChassis::SpeedPidParam(1300.0f, 1800.0f, 0.05f, 16000.0f, 0.0f, IMCREATEMENT_OF_OUT), // 左上
         OmniChassis::SpeedPidParam(1300.0f, 1800.0f, 0.0f, 16000.0f, 0.0f, IMCREATEMENT_OF_OUT), // 右上
         OmniChassis::SpeedPidParam(1300.0f, 1800.0f, 0.0f, 16000.0f, 0.0f, IMCREATEMENT_OF_OUT), // 左下
         OmniChassis::SpeedPidParam(1300.0f, 1800.0f, 0.0f, 16000.0f, 0.0f, IMCREATEMENT_OF_OUT), // 右下
